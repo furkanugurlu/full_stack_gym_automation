3f1bdf47ffa5ab0cc52164681b562e1fe39e851e
furkanugurlu <furkanugurlu3451@gmail.com>
2023-05-12 10:34:29 +0300

    cors error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const { antrenorlerRoute, branslarRoute, kategorilerRoute, esyaRoute, uyelerRoute, paketlerRoute } = require('./route')
 
 app.use(express.json())
+app.use(cors())
 app.use('/coach', antrenorlerRoute)
 app.use('/branch', branslarRoute)
 app.use('/category', kategorilerRoute)
